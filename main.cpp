eab9dc84343d15755eca7a89633e7aad6937f014
S-E-R-G-O <79408916+S-E-R-G-O@users.noreply.github.com>
2024-11-01 17:01:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include "Processing.h"
+#include "TrackingBox.h"
 #include <iostream>
 
-using namespace std;
+
 
 int main() {
+	cv::Mat frame, threshold;
+	std::vector<TrackingBox> detection, tracking;
+
 	try {
-		Processing processing("C:/Users/Main/Desktop/video1.avi", "C:/Users/Main/Desktop/Opencvtest/video2.avi");
-		processing.detectedChanges();
+		Processing processing("C:/Users/Main/Desktop/123/video1.avi", "C:/Users/Main/Desktop/123/video2.avi");
 
-
+		while (true) 
+		{
+			detection = processing.detectedChanges(frame, threshold);
+			tracking = TrackingBox::trackingCreation(detection, tracking);
+			TrackingBox::drawBoxes(frame, detection);
+			if (cv::waitKey(10) == 27)
+			{
+				break;
+			}
+			
+			imshow("Combined Video", frame); 
+			imshow("Masked Video", threshold); 
+		}
 	}
-	catch (const exception& e) {
-		cerr << e.what() << endl;
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
 		return -1;
 	}
 }
