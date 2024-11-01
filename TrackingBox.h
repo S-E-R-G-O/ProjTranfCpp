cb82785679804d174bbce9a51c27552a57296ebb
S-E-R-G-O <79408916+S-E-R-G-O@users.noreply.github.com>
2024-11-01 17:00:51 +0300

    Update TrackingBox.h

--- TrackingBox.h
+++ TrackingBox.h
@@ -2,37 +2,59 @@
 #ifndef TRK_BOX
 #define TRK_BOX
 
-#include <opencv2/opencv.hpp>
-#include <vector>
-#include <tuple>
-#include <string>
-#include <unordered_map>
+#include <opencv2/opencv.hpp> 
+#include <vector>            
+#include <tuple>             
+#include <string>            
+#include <unordered_map>    
 
+// Класс TrackingBox предназначен для представления ограничивающего прямоугольника,
+// в котором производится отслеживание объектов.
 class TrackingBox {
 public:
+    // Статическая переменная для цвета зелёного цвета (например, для отрисовки)
     static cv::Scalar GreenColor;
+    // Статическая переменная для цвета красного цвета
     static cv::Scalar RedColor;
+    // Константа для ограничения минимальной площади объекта
     static const int det_limArea = 6000;
 
+    // Конструктор класса, принимающий координаты и размеры ограничивающего прямоугольника
     TrackingBox(int x, int y, int w, int h);
+    // Метод для получения параметров формы (x, y, w, h) в виде кортежа
     std::tuple<int, int, int, int> shape() const;
-    cv::Rect rectangle() const; 
+    // Метод для получения ограничивающего прямоугольника OpenCV
+    cv::Rect rectangle() const;
+    // Метод для обработки гистограммы из предоставленного кадра
     void processHistogram(const cv::Mat& frame) const;
+    // Метод для создания множества TrackingBox на основе полученных контуров
     static std::vector<TrackingBox> createBoxes(const std::vector<std::vector<cv::Point>>& contours);
+    // Метод для отрисовки ограничивающих прямоугольников на кадре
     static cv::Mat drawBoxes(cv::Mat& frame, const std::vector<TrackingBox>& boxes);
+    // Метод для создания трекеров, используя детекции и существующие трекеры
     static std::vector<TrackingBox> trackingCreation(std::vector<TrackingBox>& detections, std::vector<TrackingBox>& trackers);
-    //1.Сравнение гистограмм: В процессе...
-    //static std::unordered_map<int, std::vector<double>> compareHistograms(const cv::Mat& new_hist);
+    // Метод для вывода информации об объекте в консоль
+    const void Print() const { std::cout << id << ":(" << x << ", " << y << ", " << w << ", " << h << ")" << std::endl; }
+   
+     // Метод для сравнения гистограмм
+    static std::unordered_map<int, std::vector<double>> compareHistograms(const cv::Mat& new_hist);
+
 
 private:
-    int x, y, w, h;
-    int id;
-    static std::tuple<cv::Mat, cv::Mat, cv::Mat> computeHist(const cv::Mat& region);
-    static void printMeanval(const cv::Mat& b_hist, const cv::Mat& g_hist, const cv::Mat& r_hist, const std::string& half);
-    static std::unordered_map<int, std::vector<cv::Mat>> track_hist;
-    static std::unordered_map<int, std::vector<cv::Mat>> del_hists;
-    static int id_counter;
+    int x, y, w, h;            // Координаты верхнего левого угла (x, y) и размеры (w, h) ограничивающего прямоугольника
+    int id;                    // Идентификатор объекта (каждый объект отслеживания имеет уникальный ID)
 
+    // Метод для вычисления гистограммы цветового региона
+    static std::tuple<cv::Mat, cv::Mat, cv::Mat> computeHist(const cv::Mat& region);
+    // Метод для вывода средних значений гистограммы(В процесе реализации)
+    static void printMeanval(const cv::Mat& b_hist, const cv::Mat& g_hist, const cv::Mat& r_hist, const std::string& half);
+
+    // Хранение гистограммы для каждого трекера
+    static std::unordered_map<int, std::vector<cv::Mat>> track_hist;
+    // Храненеие удалённых гистограмм
+    static std::unordered_map<int, std::vector<cv::Mat>> del_hists;
+    // Счётчик для создания уникальных идентификаторов объектов
+    static int id_counter;
 };
 
 #endif // TRK_BOX
