4aa5ab56f89310e72e11cba2b1ebb145df2f9bbe
S-E-R-G-O <79408916+S-E-R-G-O@users.noreply.github.com>
2024-11-12 15:56:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,30 +4,41 @@
 
 
 
+#include "Processing.h"
+#include "TrackingBox.h"
+#include <iostream>
+
+
+
 int main() {
-	cv::Mat frame, threshold;
-	std::vector<TrackingBox> detection, tracking;
+    cv::Mat frame, threshold;
+    std::vector<TrackingBox> detection, tracking;
+    setlocale(LC_ALL, "Russian");
+    try {
+        Processing processing("C:/Users/Main/Desktop/123/video1.avi", "C:/Users/Main/Desktop/123/video2.avi");
 
-	try {
-		Processing processing("C:/Users/Main/Desktop/123/video1.avi", "C:/Users/Main/Desktop/123/video2.avi");
+        while (true)
+        {
+            detection = processing.detectedChanges(frame, threshold);
+            tracking = TrackingBox::trackingCreation(detection, tracking);
+            //строим гистограмы для отслеживаемых объектов
+            for (auto& box : tracking) {
+                box.processHistogram(frame); // это будет работать, так как box - не const
+            }
 
-		while (true) 
-		{
-			detection = processing.detectedChanges(frame, threshold);
-			tracking = TrackingBox::trackingCreation(detection, tracking);
-			TrackingBox::drawBoxes(frame, detection);
-			if (cv::waitKey(10) == 27)
-			{
-				break;
-			}
-			
-			imshow("Combined Video", frame); 
-			imshow("Masked Video", threshold); 
-		}
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
+            TrackingBox::drawBoxes(frame, detection);
+
+            if (cv::waitKey(10) == 27)
+            {
+                break;
+            }
+
+            imshow("Combined Video", frame);
+            imshow("Masked Video", threshold);
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
 }
-
