a151f03bf07ad7254373acc206c050faaeac07e1
S-E-R-G-O <79408916+S-E-R-G-O@users.noreply.github.com>
2025-01-17 16:02:55 +0300

    Update Processing.h

--- Processing.h
+++ Processing.h
@@ -9,6 +9,15 @@
 // Класс для обработки видео и обнаружения изменений между кадрами
 class Processing
 {
+private:
+    cv::VideoCapture stream1; // Первый видеопоток
+    cv::VideoCapture stream2; // Второй видеопоток
+    cv::Mat previousFrame;    // Матрица для хранения предыдущего кадра
+    //cv::Mat background;
+    bool isFrame;         // Булевая переменная для отслеживания наличия кадра
+
+    // Метод для обработки одного кадра и получения объектов отслеживания
+
 public:
     // Конструктор, принимающий имена файлов видео потоков
     Processing(const std::string& fileName1, const std::string& fileName2);
@@ -18,16 +27,10 @@ public:
 
     // Метод для обнаружения изменений между двумя кадрами и возвращающий вектор отслеживаемых объектов
     std::vector<TrackingBox> detectedChanges(cv::Mat&, cv::Mat&);
-
-private:
-    cv::VideoCapture stream1; // Первый видеопоток
-    cv::VideoCapture stream2; // Второй видеопоток
-    cv::Mat previousFrame;    // Матрица для хранения предыдущего кадра
-    cv::Mat background;       // Матрица для хранения фона, который будет использоваться для сравнения
-    bool isFrame;         // Булевая переменная для отслеживания наличия кадра
-
-    // Метод для обработки одного кадра и получения объектов отслеживания
     std::vector<TrackingBox> processingFrame(cv::Mat& frame, cv::Mat& grayFrame);
+    cv::Mat background;
+
+    
 };
 
 #endif // PROCESSING_H
