c6c03c348a9792124807944a1dc6e28d408df3a8
S-E-R-G-O <79408916+S-E-R-G-O@users.noreply.github.com>
2025-02-10 14:19:48 +0300

    Update HungarianAlgorithm.h

--- HungarianAlgorithm.h
+++ HungarianAlgorithm.h
@@ -1,27 +1,23 @@
 #ifndef HUNGARIAN_ALGORITHM_H
 #define HUNGARIAN_ALGORITHM_H
 
-#include <iostream>
 #include <vector>
-
-using namespace std;
+#include <algorithm>
+#include <limits>
+#include <cmath>
 
 class HungarianAlgorithm {
-public:
-    HungarianAlgorithm();
-    ~HungarianAlgorithm();
-
-    double Solve(std::vector<std::vector<double>>& DistMatrix, std::vector<int>& Assignment);
-
 private:
     void assignmentoptimal(std::vector<int>& assignment, double* cost, const std::vector<double>& distMatrixIn, int nOfRows, int nOfColumns);
     void buildassignmentvector(std::vector<int>& assignment, const std::vector<bool>& starMatrix, int nOfRows, int nOfColumns);
     void computeassignmentcost(const std::vector<int>& assignment, double* cost, const std::vector<double>& distMatrix, int nOfRows);
-    void step2a(std::vector<int>& assignment, std::vector<double>& distMatrix, std::vector<bool>& starMatrix, std::vector<bool>& newStarMatrix, std::vector<bool>& primeMatrix, std::vector<bool>& coveredColumns, std::vector<bool>& coveredRows, int nOfRows, int nOfColumns, int minDim);
-    void step2b(std::vector<int>& assignment, std::vector<double>& distMatrix, std::vector<bool>& starMatrix, std::vector<bool>& newStarMatrix, std::vector<bool>& primeMatrix, std::vector<bool>& coveredColumns, std::vector<bool>& coveredRows, int nOfRows, int nOfColumns, int minDim);
-    void step3(std::vector<int>& assignment, std::vector<double>& distMatrix, std::vector<bool>& starMatrix, std::vector<bool>& newStarMatrix, std::vector<bool>& primeMatrix, std::vector<bool>& coveredColumns, std::vector<bool>& coveredRows, int nOfRows, int nOfColumns, int minDim);
-    void step4(std::vector<int>& assignment, std::vector<double>& distMatrix, std::vector<bool>& starMatrix, std::vector<bool>& newStarMatrix, std::vector<bool>& primeMatrix, std::vector<bool>& coveredColumns, std::vector<bool>& coveredRows, int nOfRows, int nOfColumns, int minDim, int row, int col);
-    void step5(std::vector<int>& assignment, std::vector<double>& distMatrix, std::vector<bool>& starMatrix, std::vector<bool>& newStarMatrix, std::vector<bool>& primeMatrix, std::vector<bool>& coveredColumns, std::vector<bool>& coveredRows, int nOfRows, int nOfColumns, int minDim);
+    void starMethod(std::vector<int>& assignment, std::vector<double>& distMatrix, std::vector<bool>& starMatrix, std::vector<bool>& primeMatrix, std::vector<bool>& coveredColumns, std::vector<bool>& coveredRows, int nOfRows, int nOfColumns, int minDim);
+
+public:
+    HungarianAlgorithm() {}
+    ~HungarianAlgorithm() {}
+
+    double Solve(std::vector<std::vector<double>>& DistMatrix, std::vector<int>& Assignment);
 };
 
 #endif // HUNGARIAN_ALGORITHM_H
